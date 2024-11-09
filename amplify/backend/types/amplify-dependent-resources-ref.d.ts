5ab237ed4c89e47f8d4b46a3e6f46d769779075e
Edward <edwardtxc-pm22@student.tarc.edu.my>
2024-11-09 16:56:50 +0800

    whackamole

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "whackamole": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
